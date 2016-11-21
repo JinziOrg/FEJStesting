e597696ef9fb505367480e224029cb7df27aef19
Jinzi Huang <jinzih@ca.ibm.com>
2016-11-21 14:36:02 -0500

    Fix #3

--- mochatests/test.spec.js
+++ mochatests/test.spec.js
@@ -1,5 +1,13 @@
-describe("A test suite", function() {
-   beforeEach(function() { });
-   afterEach(function() { });
-   //it('should fail', function() { expect(true).to.be.false; });
+// describe("A test suite", function() {
+//    beforeEach(function() { });
+//    afterEach(function() { });
+//    //it('should fail', function() { expect(true).to.be.false; });
+// });
+
+describe('getDiv', function() {
+    var d = document.querySelector('.box');
+
+    it('Should exist', function() {
+        expect(d.nodeName).toBe('DIV');
+    });
 });
