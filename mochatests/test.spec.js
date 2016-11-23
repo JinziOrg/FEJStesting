8121de9753d84e401b62c8fb18c6f1c9cb512201
Jinzi Huang <jinzih@ca.ibm.com>
2016-11-23 09:44:16 -0500

    test travis

--- mochatests/test.spec.js
+++ mochatests/test.spec.js
@@ -7,7 +7,7 @@
 describe('getDiv', function() {
     // var d = document.querySelector('.box');
 
-    // it('Should exist', function() {
-    //     expect(d.nodeName).toBe('DIV');
-    // });
+    it('Should exist', function() {
+       // expect(d.nodeName).toBe('DIV');
+    });
 });
